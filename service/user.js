75519263aff59a61114fe92b652cdac2c69c3f9b
Alex Vasylenko <o.vasylenko@wookieelabs.com>
2015-10-05 18:26:21 +0300

    Fix bug on users edit and now user gets error duplicate error

--- service/user.js
+++ service/user.js
@@ -152,7 +152,16 @@ isPermitted: function (action, data, author) {
       data.newPassword = 'Password was not changed';
     }
 
-    yield user.save();
+    try {
+      yield user.save();
+    } catch(e) {
+      if (e[0].code === 'ER_DUP_ENTRY') {
+        throw new errors.DuplicateError('Duplicate email');
+      } else {
+        throw new errors.Error(e[0].message);
+      }
+    }
+
     mail.sendUpdateUserProfile(data, user.email);
 
   },
