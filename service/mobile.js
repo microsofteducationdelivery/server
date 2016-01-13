d91cc62db1062a4da5f304c9501b869926dbc88a
Alex Vasylenko <o.vasylenko@wookieelabs.com>
2016-01-13 12:58:43 +0200

    Fix bug with incorrect id for concat data

--- service/mobile.js
+++ service/mobile.js
@@ -57,7 +57,7 @@ function* getFolderContent (id, cache) {
 
   items = items.concat(media.map(function (media) {
       return {
-        id: media.fakeUrl,
+        id: media.id,
         views: media.views,
         downloads: media.downloads,
         type: 'media',
