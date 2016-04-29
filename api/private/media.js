590f60ef8f0bfb7685c2ae80528e264ad3df5ae6
Alex Vasylenko <o.vasylenko@wookieelabs.com>
2016-04-29 16:20:09 +0300

    Fix bug with reload for list media

--- api/private/media.js
+++ api/private/media.js
@@ -84,7 +84,7 @@ function* changeImage() {
   var media = yield mediaService.findById(this.query.media, this.user);
 
   yield fs.writeFile('./public/preview/' + media.dataValues.fakeId + '.png', Buffer.concat(bufs));
-  this.body = media.dataValues.fakeId;
+  this.body = media.dataValues.id;
 
 }
 
