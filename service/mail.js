d64706349594ff73680404520249ff108a82786b
Alex Vasylenko <o.vasylenko@wookieelabs.com>
2016-05-19 13:11:17 +0300

    Change mailinator on nodemailer

--- service/mail.js
+++ service/mail.js
@@ -1,7 +1,7 @@
 var
   config = require('../config'),
-  SparkPost = require('sparkpost'),
-  Sparky = new SparkPost(config.mail.key),
+  directTransport = require('nodemailer-direct-transport'),
+  nodemailer = require('nodemailer'),
   _ = require('lodash')
 ;
 
@@ -11,41 +11,35 @@ module.exports = {
     var message = config.mail.credentialsText
       .replace('<password>', password)
     ;
+    var mailTransport = nodemailer.createTransport(directTransport({}));
 
-    Sparky.transmissions.send({
-      transmissionBody: {
-        content: {
-          from: config.mail.from,
-          subject: config.mail.welcomeSubject,
-          html: message
-        },
-        recipients: [
-          {address: email}
-        ]
-      }
-    }, function (err, result) {
-      (err) ? console.log(err) : console.log(result);
+    mailTransport.sendMail({
+      from: config.mail.from,
+      to: email,
+      subject: config.mail.from,
+      generateTextFromHTML: true,
+      html: message
+    }, function(err) {
+      console.log('err', err);
+      mailTransport.close();
     });
-
   },
   sendWelcomeEmail: function (user, password, email) {
     var message = config.mail.welcomeText
        .replace('<user>', user);
-
-    Sparky.transmissions.send({
-      transmissionBody: {
-        content: {
-          from: config.mail.from,
-          subject: config.mail.welcomeSubject,
-          html: message
-        },
-        recipients: [
-          { address: email }
-        ]
-      }
-    }, function (err, result) {
+    var mailTransport = nodemailer.createTransport(directTransport({}));
+    var resultFunction = function(err) {
       (err) ? console.log(err) : this.sendRegPassword(password, email);
-    }).bind(this);
+      mailTransport.close();
+    };
+
+    mailTransport.sendMail({
+      from: config.mail.from,
+      to: email,
+      subject: config.mail.welcomeSubject,
+      generateTextFromHTML: true,
+      html: message
+    }, resultFunction.bind(this));
   },
   sendRecoveryPasswordLink: function (userName, link, email) {
     var message = config.mail.recoveryText
@@ -53,19 +47,17 @@ module.exports = {
         .replace('<link>', link)
       ;
 
-    Sparky.transmissions.send({
-      transmissionBody: {
-        content: {
-          from: config.mail.from,
-          subject: config.mail.recoverySubject,
-          html: message
-        },
-        recipients: [
-          { address: email }
-        ]
-      }
-    }, function (err, result) {
-      (err) ? console.log(err) : console.log(result);
+    var mailTransport = nodemailer.createTransport(directTransport({}));
+
+    mailTransport.sendMail({
+      from: config.mail.from,
+      to: email,
+      subject: config.mail.recoverySubject,
+      generateTextFromHTML: true,
+      html: message
+    }, function(err) {
+      console.log('err', err);
+      mailTransport.close();
     });
   },
   sendUpdateUserProfile: function(data, email) {
@@ -77,19 +69,17 @@ module.exports = {
 
     messageText = messageText + '\n Regards';
 
-    Sparky.transmissions.send({
-      transmissionBody: {
-        content: {
-          from: config.mail.from,
-          subject: config.mail.changeSubject,
-          text: messageText
-        },
-        recipients: [
-          { address: email }
-        ]
-      }
-    }, function (err, result) {
-      (err) ? console.log(err) : console.log(result);
+    var mailTransport = nodemailer.createTransport(directTransport({}));
+
+    mailTransport.sendMail({
+      from: config.mail.from,
+      to: email,
+      subject: config.mail.changeSubject,
+      generateTextFromHTML: true,
+      html: messageText
+    }, function(err) {
+      console.log('err', err);
+      mailTransport.close();
     });
   }
 };
