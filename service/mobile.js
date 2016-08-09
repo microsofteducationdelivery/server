26d92f6baf3224bdf7e3eb448dfd5f990bd05fa4
Illya Klymov <xanf@xanf.me>
2016-08-09 17:30:03 +0300

    Fix datauri issues

--- service/mobile.js
+++ service/mobile.js
@@ -63,7 +63,7 @@ function* getFolderContent (id, cache) {
         type: 'media',
         subtype: typeToExtension[media.type],
         title: media.name,
-        preview: datauri('public/preview/' + media.fakeId + '.png')
+        preview: (new datauri('public/preview/' + media.fakeId + '.png')).content
       };
     })
   );
